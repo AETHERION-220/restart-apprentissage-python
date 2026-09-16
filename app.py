94f8e740509a10bc45f5e96013db544d78a75da3
AETHERION-220 <tsaphnat9@gmail.com>
2026-09-16 20:01:42 +0100

    introduction rt variables

---
+++ app.py
@@ -0,0 +1,4 @@
+print("I'm learning python")
+print(17+35*2)
+
+livre = "Gatsby le Magnifique"
---
+++ variable/_02_01_variable.py
@@ -0,0 +1,23 @@
+# Variables avec leurs valeurs
+
+nom = "Dupond"
+prenom = "Marque"
+pays = "RDC"
+
+# variables long
+marque_de_voiture = "Toyota"
+filiere_academique = "Data Science"
+jeux_favoris = "Call of Duty"
+
+#variables contenant de chiffre
+age = 22
+code_postal = 1254
+pays = "RDC"
+
+#variable pour liste
+
+profession = ["développeur", "Data Scientist"]
+cours = ["Math", "Physique", "Geometrie"]
+
+
+# c'est aussi simple que faire x = 13 dans votre cahier
