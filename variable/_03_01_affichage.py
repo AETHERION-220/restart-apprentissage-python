f8ac00565ff97f4cd9c6c88e3ad93d94cd06840b
AETHERION-220 <tsaphnat9@gmail.com>
2026-09-17 01:03:20 +0100

    notion sur l'affichage en python

---
+++ variable/_03_01_affichage.py
@@ -0,0 +1,14 @@
+# Déclaration des variables
+nom = "Jean"
+prenom = "David"
+username = "david"
+
+# Exemple 1 : concaténation avec l'opérateur +
+print(nom + username)
+print("Bonjour " + prenom + " " + nom + ", nous sommes ravis de vous voir")
+
+# Exemple 2 : affichage avec des virgules
+print("Bonjour", prenom, nom, "nous sommes ravis de vous voir")
+
+# Exemple 3 : f-string, méthode recommandée
+print(f"Bonjour {prenom} {nom}, nous sommes ravis de vous voir")
